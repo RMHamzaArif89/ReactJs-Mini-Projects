7a61cb6c55d3f3aeba1b461ca82be67101583728
RMHamzaArif89 <ranaarifisonline@gmail.com>
2024-03-05 17:40:28 +0500

    use the useState & eval() created the simple calcu lator

--- ReactMini_Pro16_to_20/src/components/calculator/Calculator.jsx
+++ ReactMini_Pro16_to_20/src/components/calculator/Calculator.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './calculator.css'
 
 function Calculator() {
+    let[val,setValue]=useState('')
+    let[answer,setAnswer]=useState('')
+
+    const changeValue=(e)=>{
+        let inpValue=e.target.value;
+        setValue(inpValue)
+    }
+    const calValue=()=>{
+      let ans=eval(val)
+      setAnswer(ans)
+    }
     return (
         <>
             <div className="cal-con">
@@ -9,7 +20,7 @@ function Calculator() {
 
                     <div className="cal-screen">
 
-                        <input type="text" className="screen" placeholder="Enter value" />
+                        <input type="text" className="screen" placeholder="Enter value" onChange={changeValue}/>
 
                     </div>
 
@@ -32,24 +43,25 @@ function Calculator() {
                         <button type="button" className="btn-cal btn-second">0</button>
 
                         <button type="button" className="btn-cal btn-third btn-dot">.</button>
-                        <button type="button" className=" btn-third btn-clear">C</button>
+                        <button type="button" className="btn-cal btn-third btn-clear">C</button>
                         <button type="button" className="btn-cal btn-log">log</button>
                         <button type="button" className="btn-cal btn-log">(</button>
                         <button type="button" className="btn-cal btn-log">)</button>
                         <button type="button" className="btn-cal btn-log">%</button>
-                        <button type="button" className=" btn-third btn-equal">=</button>
+                        <button type="button" className=" btn-third btn-equal" onClick={calValue}>=</button>
 
 
 
                     </div>
                 </div>
-            </div>
-
-
-            <div className="dis">
-                <span>Your Result:</span>
+                <div className="dis">
+                <span>Your Result:{answer}</span>
                 <span id="display"></span>
             </div>
+            </div>
+
+
+          
 
 
 
